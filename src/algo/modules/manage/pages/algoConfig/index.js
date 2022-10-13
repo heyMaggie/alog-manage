5e92cab3ad858a4479aeabac48c4d689c951bc0c
chenmj <1016219216@qq.com>
2022-10-13 15:03:29 +0800

    限制算法类型ID 位数和去掉字典

--- src/algo/modules/manage/pages/algoConfig/index.js
+++ src/algo/modules/manage/pages/algoConfig/index.js
@@ -121,6 +121,10 @@ class algoConfig extends React.PureComponent {
                         message: "请输入正整数",
                         pattern: /^\d+$/i,
                     },
+                    {
+                        validator: checkLength(2),
+                        trigger: ["change", "blur"],
+                    },
                 ],
                 component: <Input placeholder="请输入" />,
             },
@@ -379,7 +383,7 @@ class algoConfig extends React.PureComponent {
             },
             {
                 title: "算法类型ID",
-                dataIndex: "algorithmTypeIdValue",
+                dataIndex: "algorithmTypeId",
             },
             {
                 title: "算法类型名称",
