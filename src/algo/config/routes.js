cde5f3a8137ae70c1038098afe157b5191d6c29a
chenmj <1016219216@qq.com>
2022-12-06 14:57:44 +0800

    下拉框显示中文

--- src/algo/config/routes.js
+++ src/algo/config/routes.js
@@ -17,8 +17,8 @@ import axios from "axios";
 if (process.env.NODE_ENV == "development") {
     //开发环境
     // axios.defaults.baseURL = "http://192.168.2.105:20010";
-    // axios.defaults.baseURL = "http://192.168.2.105:30020";
-    axios.defaults.baseURL = "http://192.168.1.80:20010";
+    axios.defaults.baseURL = "http://192.168.2.105:30020";
+    // axios.defaults.baseURL = "http://192.168.1.80:20010";
     // axios.defaults.baseURL = "http://192.168.1.81:20010";
     // axios.defaults.baseURL = "http://192.168.1.86:20010";
 } else {
